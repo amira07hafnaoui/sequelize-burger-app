cbca65430b41da97eca1ce0cc52818f18a9c3713
amira <chikhaoui.amira07@gmail.com>
2020-01-09 01:07:39 -0600

    change the port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT ||3030;
 
 // Middleware 
 // Sets up the Express app to handle data parsing
